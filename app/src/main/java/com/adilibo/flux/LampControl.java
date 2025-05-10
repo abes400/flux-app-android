73b437a2396680576eea9dffc8323a17edb8a108
abes400 <125301699+abes400@users.noreply.github.com>
2025-05-11 00:33:55 +0300

    UPDATE `Lamp Control View`

--- app/src/main/java/com/adilibo/flux/LampControl.java
+++ app/src/main/java/com/adilibo/flux/LampControl.java
@@ -2,13 +2,32 @@ package com.adilibo.flux;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LampControl extends AppCompatActivity {
+
+    private Button backLC;
+    private TextView title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lamp_control);
+
+        backLC = findViewById(R.id.back_LC);
+        title = findViewById(R.id.title_LC);
+
+        Intent intent = getIntent();
+
+        backLC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
