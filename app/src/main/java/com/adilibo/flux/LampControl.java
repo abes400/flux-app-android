729e7386b69b5089bf1a801a17cdbb41939f659d
abes400 <125301699+abes400@users.noreply.github.com>
2025-05-14 04:20:20 +0300

    ADD Image Upload

--- app/src/main/java/com/adilibo/flux/LampControl.java
+++ app/src/main/java/com/adilibo/flux/LampControl.java
@@ -2,7 +2,13 @@ package com.adilibo.flux;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.Switch;
@@ -13,10 +19,12 @@ import com.skydoves.colorpickerview.ColorPickerView;
 import com.skydoves.colorpickerview.listeners.ColorEnvelopeListener;
 import com.skydoves.colorpickerview.sliders.BrightnessSlideBar;
 
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
 public class LampControl extends AppCompatActivity {
 
     ColorPickerView colorPickerView;
-    BrightnessSlideBar brightnessSlideBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,11 +35,13 @@ public class LampControl extends AppCompatActivity {
         LampRVModel lamp = fluxApp.getLampAt(getIntent().getIntExtra("Index", 0));
 
         Button backLC = findViewById(R.id.back_LC);
+        Button uploadPhoto = findViewById(R.id.uploadPhoto);
+        //Button resetPhoto = findViewById(R.id.resetPhoto);
         TextView title = findViewById(R.id.title_LC);
         Switch toggleLC = findViewById(R.id.toggle_LC);
         Switch autoBrightness = findViewById(R.id.autoBrightness);
         colorPickerView = findViewById(R.id.colorPickerView);
-        brightnessSlideBar = findViewById(R.id.brightnessSlide);
+        BrightnessSlideBar brightnessSlideBar = findViewById(R.id.brightnessSlide);
         colorPickerView.attachBrightnessSlider(brightnessSlideBar);
         MaterialCardView pickerCardView = findViewById(R.id.picker_card);
 
@@ -44,10 +54,36 @@ public class LampControl extends AppCompatActivity {
         backLC.setOnClickListener(v -> finish());
         toggleLC.setOnClickListener(v -> lamp.isOn = toggleLC.isChecked());
         autoBrightness.setOnClickListener(v -> lamp.auto_brightness = autoBrightness.isChecked());
+        uploadPhoto.setOnClickListener(v -> uploadPicture());
+        //resetPhoto.setOnClickListener( v -> colorPickerView.setPaletteDrawable(R.drawable.));
 
         title.setText(lamp.name);
         toggleLC.setChecked(lamp.isOn);
         autoBrightness.setChecked(lamp.auto_brightness);
         colorPickerView.setInitialColor(Color.parseColor("#" + lamp.getHexStr()));
+    }
+
+    protected void uploadPicture() {
+        Intent imageIntent = new Intent(Intent.ACTION_PICK);
+        imageIntent.setType("image/*");
+        startActivityForResult(imageIntent, 1);
+    }
+
+    @Override
+    protected void onActivityResult (int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        try {
+            // Just copied and pasted it directly from documentation, please don't ask me how it works
+            final Uri imageURi = data.getData();
+            final InputStream imageStream = getContentResolver().openInputStream(imageURi);
+            final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+            Drawable drawable = new BitmapDrawable(getResources(), selectedImage);
+            colorPickerView.setPaletteDrawable(drawable);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (RuntimeException re) {
+            re.printStackTrace();
+        }
     }
 }
