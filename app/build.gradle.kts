0d7049e521c205696b27087bceaba8157943d62b
abes400 <125301699+abes400@users.noreply.github.com>
2025-05-20 21:51:11 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,6 +32,7 @@ android {
 }
 
 dependencies {
+    implementation("io.github.georgiosgoniotakis.bluetoothwrapper:bluetoothwrapper:1.1")
     implementation("androidx.lifecycle:lifecycle-process:2.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime:2.9.0")
     implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
@@ -44,3 +45,4 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
+
